a8a6b11b4b559d388c2ac63dfd639531fcb3cdff
MarkoColic94 <markocolic@interactivepro.org.uk>
2019-10-23 13:55:17 +0200

    Starting with Class-Based Components

--- src/index.js
+++ src/index.js
@@ -1,38 +1,35 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import CommentDetail from './CommentDetail'
 import faker from 'faker'
+import ApprovalCard from './ApprovalCard'
 
 const App = () => {
     return (
         <div className="ui container comments">
-            <div className="comment">
-                <a href="/" className="avatar">
-                    <img alt="avatar" src={faker.image.avatar()} />
-                </a>
-                <div className="content">
-                    <a href="/" className="author">
-                        Sam
-                    </a>
-                    <div className="metadata">
-                        <span className="date">Today at 6:00PM</span>
-                    </div>
-                    <div className="text">Nice blog post!</div>
-                </div>
-            </div>
-            <div className="comment">
-                <a href="/" className="avatar">
-                    <img alt="avatar" src={faker.image.avatar()} />
-                </a>
-                <div className="content">
-                    <a href="/" className="author">
-                        Sam
-                    </a>
-                    <div className="metadata">
-                        <span className="date">Today at 6:00PM</span>
-                    </div>
-                    <div className="text">Nice blog post!</div>
-                </div>
-            </div>
+            <ApprovalCard>
+                <CommentDetail 
+                    author="Sam" 
+                    timeAgo="Today at 4:45" 
+                    content="Nice blog post" 
+                    imageAvatar={faker.image.avatar()}
+                />
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail 
+                    author="Alex"timeAgo="Today at 1:15"
+                    content="Horrible blog post"
+                    imageAvatar={faker.image.avatar()}
+                />
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail
+                    author="Jane"
+                    timeAgo="Yesterday at 2:25"
+                    content="I am new to blog posts"
+                    imageAvatar={faker.image.avatar()}
+                />
+            </ApprovalCard>
         </div>
     )
 };
